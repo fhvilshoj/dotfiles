2ba8f4c7e9ea575cf21b6246316401427302c7c3
Frederik Hvilshøj <frederik@cord.tech>
2024-03-18 11:00:37 +0100

    better leap configurations

--- nvim/.config/nvim/lua/config/keys.lua
+++ nvim/.config/nvim/lua/config/keys.lua
@@ -1,59 +1,60 @@
 local M = {}
 
 function M.register()
-  local nnoremap = function(t)
-    vim.keymap.set("n", t[1], t[2], t[3])
-  end
+    local nnoremap = function(t)
+        vim.keymap.set("n", t[1], t[2], t[3])
+    end
 
-  -- Scrolling
-  nnoremap({ "<C-d>", "<C-d>zz", {} })
-  nnoremap({ "<C-u>", "<C-u>zz", {} })
+    -- Scrolling
+    nnoremap({ "<C-d>", "<C-d>zz", {} })
+    nnoremap({ "<C-u>", "<C-u>zz", {} })
 
-  -- Window resizing
-  nnoremap({ "<M-j>", ":resize -2<CR>" })
-  nnoremap({ "<M-k>", ":resize +2<CR>" })
-  nnoremap({ "<M-h>", ":vertical resize -10<CR>" })
-  nnoremap({ "<M-l>", ":vertical resize +10<CR>" })
+    -- Window resizing
+    nnoremap({ "<M-j>", ":resize -2<CR>" })
+    nnoremap({ "<M-k>", ":resize +2<CR>" })
+    nnoremap({ "<M-h>", ":vertical resize -10<CR>" })
+    nnoremap({ "<M-l>", ":vertical resize +10<CR>" })
 
-  local wk = require("which-key")
-  wk.register({
-    a = { name = "formatter", b = { "<cmd>'<,'>.!genericformat --format black<CR>", mode = { "v", "n" } } },
-    b = { name = "buffer" },
-    c = { name = "code" },
-    D = { name = "DB" },
-    f = { name = "find" },
-    g = { name = "go-to" },
-    h = { name = "harpoon" },
-    m = { name = "markdown" },
-    w = {
-      name = "window", -- optional group name
-      j = { ":resize -2<CR>", "Resize (h-)" },
-      k = { ":resize +2<CR>", "Resize (h+)" },
-      h = { ":vertical resize -10<CR>", "Resize (v-)" },
-      l = { ":vertical resize +10<CR>", "Resize (v+)" },
-      ["_"] = { "<C-w>_", "Full height" },
-      ["|"] = { "<C-w>|", "Full width" },
-      ["="] = { "<C-w>=", "Distr. equally" },
-      f = { "<C-w>|<C-w>_", "Full. screen" },
-    },
-    y = {
-      name = "yank",
-      y = { '"*y', "Yank to clipboard", mode = { "n", "v" } },
-    },
-    z = { name = "zettelkasten" },
-  }, { prefix = "<leader>" })
-  wk.register({
-    ["["] = {
-      name = "Prev",
-      b = { ":bp<CR>", "Previous buffer" },
-      q = { ":cprev<CR>zz", "Previous quickfix" },
-    },
-    ["]"] = {
-      name = "Next",
-      b = { ":bn<CR>", "Next buffer" },
-      q = { ":cnext<CR>zz", "Next quickfix" },
-    },
-  })
+    local wk = require("which-key")
+    wk.register({
+        a = { name = "formatter", b = { "<cmd>'<,'>.!genericformat --format black<CR>", mode = { "v", "n" } } },
+        b = { name = "buffer" },
+        c = { name = "code" },
+        D = { name = "DB" },
+        f = { name = "find" },
+        g = { name = "go-to" },
+        h = { name = "harpoon" },
+        m = { name = "markdown" },
+        r = { name = "leap" },
+        w = {
+            name = "window", -- optional group name
+            j = { ":resize -2<CR>", "Resize (h-)" },
+            k = { ":resize +2<CR>", "Resize (h+)" },
+            h = { ":vertical resize -10<CR>", "Resize (v-)" },
+            l = { ":vertical resize +10<CR>", "Resize (v+)" },
+            ["_"] = { "<C-w>_", "Full height" },
+            ["|"] = { "<C-w>|", "Full width" },
+            ["="] = { "<C-w>=", "Distr. equally" },
+            f = { "<C-w>|<C-w>_", "Full. screen" },
+        },
+        y = {
+            name = "yank",
+            y = { '"*y', "Yank to clipboard", mode = { "n", "v" } },
+        },
+        z = { name = "zettelkasten" },
+    }, { prefix = "<leader>" })
+    wk.register({
+        ["["] = {
+            name = "Prev",
+            b = { ":bp<CR>", "Previous buffer" },
+            q = { ":cprev<CR>zz", "Previous quickfix" },
+        },
+        ["]"] = {
+            name = "Next",
+            b = { ":bn<CR>", "Next buffer" },
+            q = { ":cnext<CR>zz", "Next quickfix" },
+        },
+    })
 end
 
 return M
--- nvim/.config/nvim/lua/plugins/navigate.lua
+++ nvim/.config/nvim/lua/plugins/navigate.lua
@@ -5,10 +5,14 @@ return {
         init = function()
             local leap = require("leap")
             local user = require("leap.user")
-            leap.create_default_mappings()
             leap.opts.special_keys.prev_target = "<backspace>"
             leap.opts.special_keys.prev_group = "<backspace>"
             user.set_repeat_keys("<enter>", "<backspace>")
         end,
+        keys = {
+            { "<leader>sf", "<Plug>(leap-forward)",     mode = { "n", "o", "x" }, desc = "Leap forward" },
+            { "<leader>sb", "<Plug>(leap-backward)",    mode = { "n", "o", "x" }, desc = "Leap backward" },
+            { "<leader>sp", "<Plug>(leap-from-window)", mode = { "n", "o", "x" }, desc = "Leap other panes" },
+        },
     },
 }
