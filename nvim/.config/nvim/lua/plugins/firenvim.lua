60a245d0b60ce2d6271d8e3f6620b45cd14e0f0a
Frederik Hvilshøj <frederik@cord.tech>
2024-05-28 10:07:00 +0200

    update firenvim

--- nvim/.config/nvim/lua/plugins/firenvim.lua
+++ nvim/.config/nvim/lua/plugins/firenvim.lua
@@ -1,9 +1,31 @@
 return {
     {
         "glacambre/firenvim",
-        lazy = not vim.g.started_by_firenvim,
+        cond = not not vim.g.started_by_firenvim,
         build = function()
+            require("lazy").load({ plugins = "firenvim", wait = true })
             vim.fn["firenvim#install"](0)
+        end,
+
+        -- configure FireNvim here:
+        config = function()
+            vim.g.firenvim_config = {
+                localSettings = {
+                    [".*"] = {
+                        takeover = "never",
+                    },
+                },
+            }
+            vim.api.nvim_create_autocmd({ "BufEnter" }, {
+                group = "firenvim",
+                pattern = "colab.research.google.com_*.txt",
+                command = "set filetype=python",
+            })
+            vim.api.nvim_create_autocmd({ "BufEnter" }, {
+                group = "firenvim",
+                pattern = "github.com_*.txt",
+                command = "set filetype=markdown",
+            })
         end,
     },
 }
